016978ef17e0f22f99fafc74efa5e18f2b2265a2
Artyom Sorokoumov <artyom.sorokoumov@gmail.com>
2018-08-31 18:27:59 +0300

    added xray

--- main.cpp
+++ main.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
 
+struct xray
+{
+    xray()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+    xray(const xray &)
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+    ~xray()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+};
+
 struct closure
 {
-    int z;
-    closure(const int &z_) : z(z_)
+    xray z;
+    closure(const xray &z_) : z(z_)
     {
 
     }
     int operator()(int a, int b)
     {
-        return z + a + b;
+        return a + b;
     }
 };
 
 
 int main()
 {
-    int  z = 100;
+    xray z;
 
     closure l(z);
     std::cout << l(1, 2) << std::endl;
 
     std::cout << closure(z)(1, 2) << std::endl;
-    std::cout << [z](int a, int b) { return z + a+ b;}(1, 2) << std::endl;
+    std::cout << [z](int a, int b) { return a + b;}(1, 2) << std::endl;
 
     return 0;
 }
